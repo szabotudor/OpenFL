c8fe090c58300450fa51e37c9cc2e41c7ebd42d9
szabotudor <szabo.tudor@gmail.com>
2022-03-23 12:52:58 +0200

    Added missing include in test file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //This is a test file used when debugging the library. You shouldn't be including this in your project
 #include"openfl.hpp"
-
+#include<iostream>
 
 int main(int argc, char* args[]) {
 	OpenFL file{ RWType::READ, "hello.txt" };
